2ff740ddc2f9621c2cd6bccbe81c96dc22cb110a
soumali28 <gorai.soumali@gmail.com>
2023-01-13 22:25:12 +0530

    create mode-navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,30 @@ import React from 'react'
 import "./styles/navbar.css"
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className='navbarContainer'>
+      <div className='brand'>
+        <img src="" />
+        <h2>ManjeetKaur.Org</h2>
+        </div>
+        <div className='navbarLinks'>
+          <ul>
+            <li>
+              <a href="/home">Home</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#gallery">Gallery</a>
+            </li>
+            <li>
+              <a href="/contact">Contact Us</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <a href="/donate">Donate</a>
+        </div>
     </div>
   )
 }
