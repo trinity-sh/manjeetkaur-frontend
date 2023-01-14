39cd173a2e38331c68667d19d62864ff59fe3a5c
soumali28 <gorai.soumali@gmail.com>
2023-01-14 12:33:29 +0530

    starting ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,30 +2,30 @@ import React from 'react'
 import "./styles/navbar.css"
 const Navbar = () => {
   return (
-    <div className='navbarContainer'>
-      <div className='brand'>
+    <div className="navbarContainer">
+      <div className="brand">
         <img src="" />
-        <h2>ManjeetKaur.Org</h2>
-        </div>
-        <div className='navbarLinks'>
-          <ul>
-            <li>
-              <a href="/home">Home</a>
-            </li>
-            <li>
-              <a href="#about">About</a>
-            </li>
-            <li>
-              <a href="#gallery">Gallery</a>
-            </li>
-            <li>
-              <a href="/contact">Contact Us</a>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <a href="/donate">Donate</a>
-        </div>
+        <h2> <span className="initials">M.K </span>Foundation</h2>
+      </div>
+      <div className="navbarLinks">
+        <ul>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#gallery">Gallery</a>
+          </li>
+          <li>
+            <a href="/contact">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <a href="/donate">Donate</a>
+      </div>
     </div>
   )
 }
