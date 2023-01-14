735323fd5d8c02368e3cf1f31cf4d9bbc4de1775
soumali28 <gorai.soumali@gmail.com>
2023-01-14 20:31:53 +0530

    navbar revamped

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,46 @@
-import React from 'react'
-import "./styles/navbar.css"
-import Topbar from "./Topbar"
+import React from "react";
+import "./styles/navbar.css";
+import Topbar from "./Topbar";
+import logo from "../assets/logo.png";
 const Navbar = () => {
   return (
     <>
-    <Topbar/>
-    <div className="navbarContainer">
-      <div className="brand">
-        <img src="" />
-        <h2> <span className="initials">M.K </span>Foundation</h2>
+      <Topbar />
+      <div className="navbarContainer">
+        <div className="navbarLinks">
+          <ul>
+            <li>
+              <a href="/home">Home</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#work">Work</a>
+            </li>
+          </ul>
+        </div>
+        <div className="brand">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="navbarLinks">
+          <ul>
+            <li>
+              <a href="#gallery">Gallery</a>
+            </li>
+            <li>
+              <a href="/contact">Contact Us</a>
+            </li>
+            <li>
+              <a className="donateBtn" href="/donate">
+                Donate
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="navbarLinks">
-        <ul>
-          <li>
-            <a href="/home">Home</a>
-          </li>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#gallery">Gallery</a>
-          </li>
-          <li>
-            <a href="/contact">Contact Us</a>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <a href="/donate">Donate</a>
-      </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
