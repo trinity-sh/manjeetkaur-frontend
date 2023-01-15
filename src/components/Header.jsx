2a674fa7b665b34cb4b543bce2a7357a53b229e7
Debdeep1 <deb.baner.1@gmail.com>
2023-01-15 11:04:29 +0530

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,9 @@ import "./styles/header.css"
 function Header() {
   return (
     <div>
-      <div className="header"></div>
+      <div className="header">
+        <div className="header-text"><h2>Let's take one step towards Humanity!</h2><h2>Let's take one step towards Humanity!</h2></div>
+      </div>
       <SvgComponent />
     </div>
   );
