272141a18124288cbc8caa80baa3e4b4de28d3b7
jayzsh <jay.dnb@outlook.in>
2023-02-19 01:59:58 +0530

    Change / target to <Boiler2 /> from <Home />

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Navbar from "./components/Navbar"
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Boiler2 from "./pages/Boiler2";
+
 const App = () => {
   return (
     <BrowserRouter>
-    <Navbar />
+      {/* <Navbar /> */}
       <Routes>
-        <Route path='/' element={<Home/>} />
+        {/* <Route path='/' element={<Home/>} /> */}
+        {/* <Route path='/' element={<Boiler />} /> */}
+        <Route path='/' element={<Boiler2 />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
