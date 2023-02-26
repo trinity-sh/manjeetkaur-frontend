59206d061ec5dcd6caa4991aeb0b18404e1b64f9
jayzsh <jay.dnb@outlook.in>
2023-02-26 16:22:29 +0530

    Disable Header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Header from '../components/Header'
 import "./styles/home.css"
+
 const Home = () => {
   return (
     <div> 
-      <Header />
+      {/* <Header /> */}
     </div>
   )
 }
