b249842dd0fa0e3e6e74962dbc273da4ad19d7c2
jayzsh <jay.dnb@outlook.in>
2023-02-26 18:52:10 +0530

    Modify selection hover animations

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import MKF from "../assets/mkf-favicon3.png";
 
 const Navbar = () => {
     return (
-        <div className="w-full h-20 shadow-md flex items-center justify-center">
+        <div className="w-full h-20 shadow-md flex items-center justify-center navbg">
             <div className="flex flex-row justify-between items-center w-full max-w-[1300px] px-2 h-full">
                 <div className="group px-3 overflow-visible h-20 font-bold text-5xl sprosans flex items-center duration-[300ms] hover:bg-pink-500">
                     <span className="group-hover:text-white group-hover:h-30 text-pink-500 sprosans h-full flex items-center">MK</span>F
@@ -21,11 +21,16 @@ const Navbar = () => {
                         "FAQ",
                         "Contact Us",
                     ].map((i) => (
-                        <div className="group px-4 font-semibold sprosans h-full flex items-center hover:bg-pink-500 duration-300">
-                            <span className="group-hover:text-white duration-[0ms] group-hover:border-b-[1.5px]">{i}</span>
+                        <div className="relative group px-4 font-semibold sprosans h-full flex flex-col justify-center items-center hover:bg-pink-500 duration-300 hover:border-b-[4px] border-b-[2px] border-pink-500">
+                            <span className="group-hover:text-white duration-[200ms] group-hover:border-b-[1.5px] group-hover:relative group-hover:-translate-y-[4px]">{i}</span>
+                            <div className="absolute left-1/2 top-[80%] text-white group-hover:visible transition -translate-x-1/2 -translate-y-1/2">⌄</div>
                         </div>
                     ))}
                 </div>
+
+                <button className="px-4 font-semibold outline-none mx-4 sprosans flex items-center rounded-md bg-yellow-400 hover:bg-yellow-500 active:bg-yellow-600 duration-300 h-10">
+                    Donate ❤
+                </button>
             </div>
         </div>
     );
