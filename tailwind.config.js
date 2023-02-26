45375c474301e6c4978f17202b3efdf0f29a669c
jayzsh <jay.dnb@outlook.in>
2023-02-26 16:21:25 +0530

    Add module theme screen config; min-width & max-width

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,26 @@ module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
         extend: {},
+
+        screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+
+        'mob': { max: '1023px' },
+        // => @media (max-width: 1024px) { ... }
+
+        'wide': '1024px',
+        // => @media (min-width: 1025px) { ... }
+
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+        }
     },
     plugins: [],
 };
