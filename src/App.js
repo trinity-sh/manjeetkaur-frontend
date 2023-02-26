85479e6701179ec5dcc9726a6c80b29b445400c6
jayzsh <jay.dnb@outlook.in>
2023-02-26 16:20:34 +0530

    chore: Reformat

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Navbar from "./components/Navbar"
-const App = () => {
-  return (
-    <BrowserRouter>
-    <Navbar />
-      <Routes>
-        <Route path='/' element={<Home/>} />
-      </Routes>
-    </BrowserRouter>
-  )
-}
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import "./App.css";
 
-export default App
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default App;
