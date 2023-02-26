46c43eb6739834e8ed719670df3e0c540ff9688f
jayzsh <jay.dnb@outlook.in>
2023-02-26 16:22:55 +0530

    Add basic navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,48 +1,34 @@
 import React from "react";
 import "./styles/navbar.css";
-import Topbar from "./Topbar";
-import logo from "../assets/logo.png";
+import MKF from "../assets/mkf-favicon3.png";
+
 const Navbar = () => {
-  return (
-    <div className="navbar">
-      <div className="nav">
-        <Topbar />
-        <div className="navbarContainer">
-          <div className="navbarLinks">
-            <ul>
-              <li>
-                <a href="/home">Home</a>
-              </li>
-              <li>
-                <a href="#about">About</a>
-              </li>
-              <li>
-                <a href="#work">Work</a>
-              </li>
-            </ul>
-          </div>
-          <div className="brand">
-            <img src={logo} alt="logo" />
-          </div>
-          <div className="navbarLinks">
-            <ul>
-              <li>
-                <a href="#gallery">Gallery</a>
-              </li>
-              <li>
-                <a href="/contact">Contact Us</a>
-              </li>
-              <li>
-                <a className="donateBtn" href="/donate">
-                  Donate
-                </a>
-              </li>
-            </ul>
-          </div>
+    return (
+        <div className="w-full h-20 shadow-md flex items-center justify-center">
+            <div className="flex flex-row justify-between items-center w-full max-w-[1300px] px-2 h-full">
+                <div className="group px-3 overflow-visible h-20 font-bold text-5xl sprosans flex items-center duration-[300ms] hover:bg-pink-500">
+                    <span className="group-hover:text-white group-hover:h-30 text-pink-500 sprosans h-full flex items-center">MK</span>F
+                </div>
+
+                <div className="h-full w-fit text-right mob:hidden flex items-center">
+                    {[
+                        "Home",
+                        "Mission",
+                        "Gallery",
+                        "Volunteer",
+                        "Blogs",
+                        "Partners & Sponsors",
+                        "FAQ",
+                        "Contact Us",
+                    ].map((i) => (
+                        <div className="group px-4 font-semibold sprosans h-full flex items-center hover:bg-pink-500 duration-300">
+                            <span className="group-hover:text-white duration-[0ms] group-hover:border-b-[1.5px]">{i}</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Navbar;
