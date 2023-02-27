7efd294fff7a429cd078de58e8ad19c891fcdb86
jayzsh <jay.dnb@outlook.in>
2023-02-27 11:29:47 +0530

    Remove SvgComponent

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import SvgComponent from "./SvgComp";
-import "./styles/header.css"
+import "./styles/header.css";
+
 function Header() {
+  
   return (
     <div>
-      <div className="header">
-        {/* <div className="header-text"><h2>Let's take one step towards Humanity!</h2><h2>Let's take one step towards Humanity!</h2></div> */}
+      <div className="">
+        
       </div>
-      <SvgComponent />
     </div>
   );
 }
