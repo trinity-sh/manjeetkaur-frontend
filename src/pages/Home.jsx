ae2caf5386becc3e104d1a54b29cb17bef075a25
jayzsh <jay.dnb@outlook.in>
2023-02-27 11:31:48 +0530

    Resolve merge conflict

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,11 @@ import About from "../components/About";
 import Header from "../components/Header";
 
 const Home = () => {
-    return (
-        <div>
-            {/* <Header /> */}
-            <About />
-        </div>
-    );
-};
+  return (
+    <div> 
+      <Header />
+    </div>
+  )
+}
 
 export default Home;
