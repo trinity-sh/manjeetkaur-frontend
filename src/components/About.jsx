4b7bfe7f508209a12b08139cae6b05fa5cf4ec2f
soumali28 <gorai.soumali@gmail.com>
2023-03-05 20:09:59 +0530

    minor fix in abour us

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,42 +3,41 @@ import React from "react";
 function About() {
     return (
         <div id="about">
-            <div className="flex flex-col items-center justify-center h-auto md:h-screen px-7">
-                <div className="max-w-5xl mx-auto">
+            <div className="flex flex-col items-center justify-center h-auto md:h-full px-7">
+                <div className="lg:p-20 md:pt-24 md:px-12 mx-auto">
                     <h2 className="text-xl mb-8 font-semibold leading-8 tracking-tight text-pink-600">
                         About Us
                     </h2>
-                    <div className="flex flex-col md:flex-row items-center">
-                    <div className="md:w-1/2 mb-4 md:mb-0 mr-4 md:mr-8">
-    <img
-        src="https://www.daysoftheyear.com/wp-content/uploads/international-day-of-charity-1.jpg"
-        alt="Company logo"
-        className="rounded-xl w-full sm:max-w-xs md:max-w-none md:w-80 h-64 sm:h-48 md:h-auto object-cover object-center"
-    />
-</div>
-<div className="md:w-3/4">
-    <p className="text-base md:text-lg lg:text-xl text-gray-700 mb-4 md:ml-4">
-        Lorem ipsum dolor sit amet, consectetur
-        adipiscing elit. Sed ultricies enim et risus
-        vulputate, eu tincidunt ex blandit. Sed in nunc
-        commodo, bibendum odio et, varius nisi. Vivamus
-        mollis velit nec sem blandit bibendum. Ut vel
-        dolor ullamcorper, faucibus lectus sed, auctor
-        velit. Nullam porta hendrerit lectus, vel dictum
-        nisi mollis nec.
-    </p>
-    <p className="text-base md:text-lg lg:text-xl text-gray-700 mb-4 md:ml-4">
-        Sed rutrum vestibulum ante, vel aliquet magna
-        fermentum ut. Sed lacinia turpis ut mauris
-        dapibus aliquam. Aenean vel sapien enim. Nullam
-        sit amet magna sem. Donec tristique enim vel
-        nunc dictum, ac elementum quam consectetur.
-        Integer eget semper mi, nec fringilla nulla.
-        Donec euismod enim sit amet orci pharetra, ut
-        sagittis quam sagittis.
-    </p>
-</div>
-
+                    <div className="flex justify-between flex-col md:flex-row items-center">
+                        <div className="lg:w-3/4 md:w-1/2 md:h-96 mb-4 md:mb-0 mr-4 md:mr-8">
+                            <img
+                                src="https://www.daysoftheyear.com/wp-content/uploads/international-day-of-charity-1.jpg"
+                                alt="Company logo"
+                                className="rounded-xl w-full object-cover h-full"
+                            />
+                        </div>
+                        <div className="md:w-3/4">
+                            <p className="text-base md:text-lg lg:text-xl text-gray-700 mb-4 md:ml-4">
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit. Sed ultricies enim et risus
+                                vulputate, eu tincidunt ex blandit. Sed in nunc
+                                commodo, bibendum odio et, varius nisi. Vivamus
+                                mollis velit nec sem blandit bibendum. Ut vel
+                                dolor ullamcorper, faucibus lectus sed, auctor
+                                velit. Nullam porta hendrerit lectus, vel dictum
+                                nisi mollis nec.
+                            </p>
+                            <p className="text-base md:text-lg lg:text-xl text-gray-700 mb-4 md:ml-4">
+                                Sed rutrum vestibulum ante, vel aliquet magna
+                                fermentum ut. Sed lacinia turpis ut mauris
+                                dapibus aliquam. Aenean vel sapien enim. Nullam
+                                sit amet magna sem. Donec tristique enim vel
+                                nunc dictum, ac elementum quam consectetur.
+                                Integer eget semper mi, nec fringilla nulla.
+                                Donec euismod enim sit amet orci pharetra, ut
+                                sagittis quam sagittis.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
