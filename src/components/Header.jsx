6838183b005aea50fade984ba763549ba6bef2b8
soumali28 <gorai.soumali@gmail.com>
2023-03-05 19:31:02 +0530

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import logo1 from "../assets/logosmod12.png";
+import logo1 from "../assets/logosmod11.png";
 import { FaFacebook } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
@@ -8,71 +7,33 @@ import { FaInstagram } from "react-icons/fa";
 function Header() {
     return (
         <div>
-            <section class="px-3 py-5 bg-neutral-100 lg:py-10">
+            <section class="px-3 py-5 bg-white-100 lg:py-10 relative">
                 <div class="grid lg:grid-cols-2 items-center justify-items-center gap-5">
-                    <div class="order-2 lg:order-1 flex flex-col justify-center items-center">
-                        <p class="text-xl font-bold md:text-1xl">
-                            <a
-                                target="_blank"
-                                href="https://www.facebook.com/groups/ManjeetKaurFoundation"
-                                class="mb-4"
-                            >
-                                <button className="text-white px-4 py-2 rounded-md">
-                                    <FaFacebook className="inline-block mr-2 text-blue-600" />
-                                </button>
-                                <button className="bg-blue-600 hover:bg-blue-700 rounded-full text-white font-bold py-2 px-4 flex items-center justify-center">
-                                    ManjeetKaurFoundation
-                                </button>
-                            </a>{" "}
-                            <br />
-                            <a
-                                target="_blank"
-                                href="https://www.instagram.com/ManjeetKaurFoundation"
-                                class="mb-4"
-                            >
-                                <button className="text-white px-4 py-2 rounded-md">
-                                    <FaInstagram className="inline-block mr-2 text-pink-500" />
-                                </button>
-                                <button className="bg-pink-500 hover:bg-pink-600 rounded-full text-white font-bold py-2 px-4 flex items-center justify-center">
-                                    @ManjeetKaurFoundation
-                                </button>
-                            </a>
-                            <br />
-                            <a
-                                target="_blank"
-                                href="https://www.twitter.com/ManjeetKaurOrg"
-                                class="mb-4"
-                            >
-                                <button className="text-white px-4 py-2 rounded-md">
-                                    <FaTwitter className="inline-block mr-2 text-blue-400" />
-                                </button>
-                                <button className="bg-blue-400 hover:bg-blue-500 rounded-full text-white font-bold py-2 px-4 flex items-center justify-center">
-                                    #MANJEETKAURORG
-                                </button>
-                            </a>
-                        </p>
-                    </div>
-                    <div class="order-1 lg:order-2">
+                    <div class="order-1 lg:order-2 w-full">
                         <img
-                            class="h-80 w-80 object-cover lg:w-[500px] lg:h-[500px]"
+                            class="h-100 w-full object-cover lg:w-[500px] lg:h-[500px]"
                             src={logo1}
                             alt=""
                         />
                     </div>
                 </div>
-            </section>
-            <div className=" bg-pink-500 overflow-hidden shadow-lg backdrop-filter backdrop-blur-md">
-                <div className="p-4">
-                    <h2 className="text-3xl font-bold text-white mb-2">
-                        Welcome to MANJEET KAUR FOUNDATION
-                    </h2>{" "}
-                    <p className="text-lg text-white">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Sed ultricies enim et risus vulputate, eu tincidunt ex
-                        blandit.
-                    </p>
+                <div className="absolute top-0 sm:m-3 flex">
+                    <a href="" className="p-2">
+                        <FaFacebook className=" md:text-3xl text-blue-600" />
+                    </a>
+                    <a href="" className="p-2">
+                        <FaInstagram className="md:text-3xl text-pink-400" />
+                    </a>
+                    <a href="" className="p-2">
+                        <FaTwitter className="md:text-3xl text-blue-400" />
+                    </a>
                 </div>
-            </div> 
+            </section>
+            <div>
+                <p className="custom-font pt-24 p-b-4 font-bold text-center sm:text-2xl md:text-3xl text-pink-700">
+                    Laboris ullamco consectetur aliqua sit dolore nostrud quis.
+                </p>
+            </div>
         </div>
     );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./styles/navbar.css";
 import "../App.css";
 import Logo from "../assets/logo.png";
 
