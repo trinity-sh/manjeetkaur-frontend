b1b08da79f9f35770a9f12366fd0b076703913fc
jayzsh <jay.dnb@outlook.in>
2023-04-19 21:51:12 +0530

    Change to dark: nav top-bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,31 @@
 import React from "react";
 import "./styles/header.css";
-import logo1 from "../assets/heroimg.jpg";
-import logo2 from "../assets/newdesign11.png";
-import { FaFacebook } from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
-import MKFLogo from "../assets/logo-highres.png"
+import MKFLogo from "../assets/logo-highres-bubble.png";
 
 function Header() {
     return (
-        <div>
-            <div className="herobg h-[calc(100vh-5rem)] flex items-center justify-center">
-                <img className="wide:w-[40%]" src={MKFLogo} alt="MKF Logo" />
+        <>
+            <div className="flex flex-col items-center justify-center overflow-visible">
+                <div className="herobg h-[calc(100vh-400px)] flex items-center justify-center w-[100%]">
+                    <img
+                        className="wide:w-[30%]"
+                        src={MKFLogo}
+                        alt="MKF Logo"
+                    />
+                </div>
+
+                <div className="z-[5] w-full max-w-[1000px] p-6 rounded-3xl bg-white absolute bottom-0 translate-y-[-4.3rem] shadow-2xl">
+                    <p className="custom-font font-bold text-center text-xl text-pink-700">
+                        "Service to humanity is not just a noble gesture, it is
+                        the essence of our existence. Every act of kindness, no
+                        matter how small, has the power to transform someone's
+                        life. Let us all strive to be the change we want to see
+                        in the world by serving humanity with love, compassion,
+                        and dedication."
+                    </p>
+                </div>
             </div>
-            <div>
-                <p className="custom-font pb-20 md:pb-2 md:pt-24 font-bold text-center text-lg sm:text-2xl md:text-3xl text-pink-700">
-                    "Service to humanity is not just a noble gesture, it is the
-                    essence of our existence. Every act of kindness, no matter
-                    how small, has the power to transform someone's life. Let us
-                    all strive to be the change we want to see in the world by
-                    serving humanity with love, compassion, and dedication."
-                </p>
-            </div>
-        </div>
+        </>
     );
 }
 
