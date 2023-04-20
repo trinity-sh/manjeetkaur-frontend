778bd333cf80b11e6858844363cd3ec180db6a33
jayzsh <jay.dnb@outlook.in>
2023-04-20 20:10:59 +0530

    Upgrade header x3: resize event, urgent post, close btn

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,17 +12,20 @@ import S6 from "../assets/donation-6.jpg";
 import S7 from "../assets/donation-7.jpg";
 import S8 from "../assets/donation-8.jpeg";
 import S9 from "../assets/donation-9.jpeg";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
     /*
      * @warning: All slider image items must be of 848 x 523 (constant height) resolution for a consistent viewing experinece
-     *       across devices of all width
+     *       across all device widths
      * */
     const slides = [S1, S2, S3, S4, S5, S6, S7, S8, S9];
+
     const [centerSlidePercentage, setCenterSlidePercentage] = useState(80);
+    const navigate = useNavigate();
 
     useEffect(() => {
-        window.addEventListener("resize", () => {
+        const onResize = () => {
             const vw = Math.max(
                 document.documentElement.clientWidth || 0,
                 window.innerWidth || 0
@@ -30,19 +33,21 @@ function Header() {
 
             if (vw < 1024) {
                 setCenterSlidePercentage(100);
-
             } else if (vw >= 1024) {
                 setCenterSlidePercentage(80);
             }
-        });
+        };
+
+        window.addEventListener("resize", () => onResize());
+        onResize();
     }, []);
 
     return (
         <>
-            <div className="flex flex-col items-center justify-center overflow-visible mob:mb-44 relative">
+            <div className="flex flex-col items-center justify-center overflow-visible relative">
                 <div
                     id="carousel-cont"
-                    className="h-[calc(100vh-400px)] mob:h-[calc(100vh-5rem)] flex items-center justify-center w-[100%] relative"
+                    className="h-[calc(100vh-300px)] mob:h-[calc(100vh-5rem)] flex items-center justify-center w-[100%] relative"
                 >
                     <Carousel
                         className="h-full z-0"
@@ -61,7 +66,7 @@ function Header() {
                         interval={5000}
                     >
                         {slides.map((i) => (
-                            <div className="h-[calc(100vh-400px)] mob:h-[calc(100vh-5rem)] overflow-hidden">
+                            <div className="h-[calc(100vh-300px)] mob:h-[calc(100vh-5rem)] overflow-hidden">
                                 <img
                                     className="min-h-full object-cover"
                                     src={i}
@@ -70,23 +75,94 @@ function Header() {
                             </div>
                         ))}
                     </Carousel>
-                    <div className="absolute h-full w-full top-0 left-0 wide:shadow-inset-t" />
-                    <div className="wide:hidden absolute flex items-center justify-center h-full w-full top-0 left-0">
+                    <div id="dark-side" className="transition duration-300 absolute h-full w-full top-0 left-0 wide:shadow-inset-t bg-black bg-opacity-[0.6]" />
+                    <div className="absolute flex flex-col items-center justify-center h-full w-full top-0 left-0">
                         <img
-                            className="w-[70%] trans-png-dropshadow"
+                            id="img-behind-urgent"
+                            className="hidden wide:h-[40%] mob:w-[70%] trans-png-dropshadow self-cemter"
                             src={MKFLogo}
                             alt="MKF Logo"
                         />
+                        <p
+                            id="header-urgent"
+                            className="transition-transform duration-1000 overflow-visible relative sprosans text-white text-6xl mob:text-4xl max-w-[1000px] mob:mx-8 bg-black p-8 rounded-lg bg-opacity-60 flex flex-col backdrop-blur-md"
+                        >
+                            <div
+                                onClick={() => {
+                                    document.getElementById("header-urgent").style.transitionProperty = "all";
+                                    document.getElementById("header-urgent").style.transitionDuration = "800ms";
+                                    document.getElementById("header-urgent").style.display = "none";
+                                    document.getElementById("img-behind-urgent").style.display = "block";
+                                }}
+                                className="absolute bg-gray-900 opacity-70 backdrop-blur-lg text-black hover:text-red-700 rounded-full transition-all hover:opacity-100 top-0 right-0 w-[38px] h-[38px] flex items-center justify-center hover:bg-gray-900 text-xs font-extrabold p-3 translate-x-[10px] -translate-y-[10px]"
+                            >
+                                ✖
+                            </div>
+                            <img
+                                className="wide:hidden h-[10vh] my-5 trans-png-dropshadow self-start mob:self-center"
+                                src={MKFLogo}
+                                alt="MKF Logo"
+                            />
+                            <span className="font-bold sprosans wide:bg-gray-700 wide:backdrop-blur-lg wide:bg-opacity-40 w-fit mob:block">
+                                &nbsp;Help Us Fight&nbsp;
+                            </span>
+                            <span className="sprosans text-pink-600 font-bold bg-gray-800 bg-opacity-60 w-fit">
+                                &nbsp;
+                                <span className="text-yellow-400">
+                                    Summer
+                                </span>{" "}
+                                Hunger&nbsp;
+                            </span>
+                            <p className="sprosans text-base text-white font-light">
+                                <br />
+                                <b>
+                                    Summer hunger is a critical issue affecting
+                                    families across the country.
+                                </b>{" "}
+                                Many children rely on free or reduced-price
+                                school meals to access nutritious food during
+                                the year. But when summer arrives, these vital
+                                resources disappear, leaving families struggling
+                                to make ends meet.
+                                <br />
+                                <br />
+                                <b>
+                                    You can help end summer hunger by supporting
+                                    our charity organization.
+                                </b>{" "}
+                                Your donation can provide a nutritious meal to a
+                                child in need, helping them to grow, learn, and
+                                thrive. With your support, we can make a
+                                difference and ensure that no child goes hungry
+                                this summer.{" "}
+                                <b>Please consider donating today.</b> Thank you
+                                for your generosity.
+                            </p>
+                            <div className="flex flex-row space-x-8 my-5">
+                                <button
+                                    className="monospace text-black w-28 text-center font-semibold outline-none sprosans flex items-center justify-center rounded-md text-sm bg-yellow-400 hover:bg-yellow-300 active:bg-yellow-500 duration-300 h-10"
+                                    onClick={() => navigate("/donate")}
+                                >
+                                    Donate ❤
+                                </button>
+                                <button
+                                    className="monospace text-sm w-36 text-center font-semibold outline-none sprosans flex items-center justify-center rounded-md border-yellow-400 hover:border-yellow-600 active:border-yellow-800 border bg-transparent duration-300 h-10"
+                                    onClick={() => navigate("/about")}
+                                >
+                                    Learn more...
+                                </button>
+                            </div>
+                        </p>
                     </div>
                 </div>
 
-                <div className="z-[5] rounded-lg mx-8 max-w-[900px] p-6 bg-white absolute bottom-0 translate-y-1/2 shadow-xl transition-all duration-300 hover:shadow-2xl">
+                {/* <div className="z-[5] rounded-lg mx-8 max-w-[900px] p-6 bg-white absolute bottom-0 translate-y-1/2 shadow-xl transition-all duration-300 hover:shadow-2xl">
                     <p className="custom-font font-bold text-center text-xl text-pink-700">
                         "Charity is the recognition of the fact that we have
                         more in common with those who are different from us than
                         those who are similar."
                     </p>
-                </div>
+                </div> */}
             </div>
         </>
     );
