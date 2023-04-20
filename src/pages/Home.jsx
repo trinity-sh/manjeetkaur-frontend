46fd47118dc207540afd6113fa56e2dcd8e8dc85
jayzsh <jay.dnb@outlook.in>
2023-04-20 20:13:20 +0530

    Change to flex box layout to support centering

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import Header from "../components/Header";
 
 const Home = () => {
     return (
-        <div>
+        <div className="flex flex-col">
             <Header />
             <About />
             <Gallery />
